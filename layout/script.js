418ee4e8561ca00c908cd89e447cdbb0a8a6b5ec
Matthew Paz <paz.matthew@me.com>
2016-11-22 15:45:57 -0500

    add basic login form

--- layout/script.js
+++ layout/script.js
@@ -1,1 +1,17 @@
 console.log('script.js connected')
+
+window.onload = ( () => {
+
+  let showLogin = document.querySelector('#showLogin')
+  let login = document.querySelector('#loginDiv');
+
+  showLogin.addEventListener('click', showLoginFunc);
+
+  function showLoginFunc() {
+    console.log('clicked')
+    console.log(login)
+    login.style.display = 'block';
+    showLogin.style.display = 'none';
+  }
+
+})
