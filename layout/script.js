62cab3c45c51e3fdcaaca15800467f5c6c5ccf71
Matthew Paz <paz.matthew@me.com>
2016-11-22 16:26:28 -0500

    finish nav example

--- layout/script.js
+++ layout/script.js
@@ -3,15 +3,26 @@ console.log('script.js connected')
 window.onload = ( () => {
 
   let showLogin = document.querySelector('#showLogin')
-  let login = document.querySelector('#loginDiv');
+  let login = document.querySelector('#loginSignup');
+  let modalButton = document.querySelectorAll('.modalButton');
+  let welcome = document.querySelector('#welcome');
 
-  showLogin.addEventListener('click', showLoginFunc);
+  showLogin.addEventListener('click', showLoginSignupFunc);
 
-  function showLoginFunc() {
-    console.log('clicked')
-    console.log(login)
+  function showLoginSignupFunc() {
     login.style.display = 'block';
     showLogin.style.display = 'none';
   }
+
+  function hideLoginForm() {
+    modalButton.forEach( (button) => {
+      button.addEventListener('click', () => {
+        login.style.display = 'none';
+        welcome.style.display = 'block'
+      })
+    })
+  }
+
+  hideLoginForm();
 
 })
