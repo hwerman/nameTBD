823c3ff3399a415110e6f64b36837f11d8dfe809
Daniel Pease <daniel.j.pease@gmail.com>
2016-11-23 16:05:57 -0500

    finish auth

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 const db = require('../lib/db');
+const jwt = require('jsonwebtoken');
 
 function comparePassword (req, res, next) {
   db.one('SELECT * from grojjUsers WHERE grojjUsers.username = $/username/ AND grojjUsers.password = $/password/;', req.body)
-    .then((data) => {      
+    .then((data) => {
     if (!data) {
         res.status(500).send('Login Failed');
       } else {
-        res.status(200).json({message: 'Login Successful'});
+        var myToken = jwt.sign({ username: req.body.username }, 'taco3000');
+        res.status(200).json(myToken);
       }
     })
     .then(() => {
