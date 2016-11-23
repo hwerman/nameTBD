d7cdddf14ca8a28b8446e0a876d9113e3acc88ae
Hadas Werman <hjwerman@gmail.com>
2016-11-23 09:41:21 -0500

    schema

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,26 @@
+DROP TABLE IF EXISTS grojjstorefronts;
+
 BEGIN;
+
+-- CREATE TABLE grojjstorefronts (
+--   id SERIAL PRIMARY KEY,
+--   address TEXT NOT NULL,
+--   borough TEXT NOT NULL,
+--   zipcode TEXT NOT NULL,
+--   sellername VARCHAR NOT NULL,
+--   sellerid TEXT NOT NULL
+-- );
+
+CREATE TABLE grojjitems(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  price VARCHAR NOT NULL,
+  condition VARCHAR NOT NULL,
+  likes INT NOT NULL DEFAULT 0,
+  url TEXT,
+  sellerid TEXT NOT NULL
+);
+
+-- sellername, sellerid
 
 COMMIT;
--- lib/seeds.sql
+++ lib/seeds.sql
@@ -1,3 +1,16 @@
 BEGIN;
 
-COMMIT;
+INSERT INTO grojjitems(id, name, price, condition, likes ,url, sellerid) VALUES
+
+(1, 'Red Vintage Armchair', '$', 'Barely Used', 0, '', 1),
+(2, 'I Love Lucy Mug', '$$', 'Used', 0, '',1 ),
+(3, 'My walkman from 1996', '$', 'Used', 0, '', 1),
+(4, 'Motorola Razr', '$', 'Used', 0, '', 2),
+(5, 'John Deere hat' '$$', 'New', 0, '', 2),
+(6, 'Ball and 10 jacks' '$', 'Used', 0, '', 2),
+(7, 'Fig Newtons', '$', 'New', 0, '', 3),
+(8, 'Bugs Bunny Looney Tunz jersey', '$$$', 'New', 0, '', 3),
+(9, 'Hunger Games book trilogy', '$', 'Barely Used', 0, '', 4),
+(10, 'Game of Risk', '$$', 'Used', 0, '', 4)
+
+COMMIT:
