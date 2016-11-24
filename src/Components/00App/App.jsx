b22e3f58e7b327be36e8e2099b39bbfb6a87d15b
Daniel Pease <daniel.j.pease@gmail.com>
2016-11-23 20:51:01 -0500

    evening merge

--- src/Components/00App/App.jsx
+++ src/Components/00App/App.jsx
@@ -3,6 +3,28 @@ import './App.css'
 
 class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      searchForm: ''
+    };
+  }
+
+  revealer(e) {
+    let children = e.currentTarget.firstElementChild.parentNode.childNodes
+    console.log(children.length)
+
+    for (let key in children) {
+      console.log(key)
+    }
+    e.currentTarget.nextSibling.style.display = 'block';
+  }
+
+  hider(e) {
+    e.currentTarget.nextSibling.style.display = 'none';
+  }
+
   render(){
     return (
       <div>
@@ -10,12 +32,16 @@ class App extends Component {
           <h1>Grojj.</h1>
           <button>Login or Sign Up</button>
           <nav>
-            <div className="nButtons">Search</div>
-            <div className="nButtons">myStoreFront</div>
-            <div className="nButtons">Messages</div>
+            <div onMouseEnter={this.revealer} onMouseOut={this.hider}>Search
+              <input type="text" placeholder="search"/>
+              <button>Go!</button>
+            </div>
+            <div>myStoreFront</div>
+            <div>Messages</div>
           </nav>
         </header>
         <main>
+
         </main>
         <footer>
           <div>copyright nonsense, about us</div>
