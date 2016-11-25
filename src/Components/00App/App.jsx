ae8a6ece8ccf8360eaeba4a470f9c6703cfbb048
Cecil Lee <cmjlee3@gmail.com>
2016-11-25 18:14:27 -0500

    work in progress for multiple pins

--- src/Components/00App/App.jsx
+++ src/Components/00App/App.jsx
@@ -2,27 +2,109 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Map from './Maps.jsx';
+// import Places from './Places.jsx';
+// import superagent from 'superagent';
 // import './normalize.css';
 // import style from './App.css';
 
+// class GMap extends React.Component {
+//   constructor() {
+//     super();
 
-// class App extends Component {
-//   render(){
-//     return (
-//       <div>
-//         This is the App!!
+//    this.state = { zoom: 10 };
+
+//   }
 
 
+//   static propTypes() {
+//     initialCenter: React.PropTypes.objectOf(React.PropTypes.number).isRequired
+//   }
+
+//   render() {
+//     return <div className="GMap">
+//       <div className='UpdatedText'>
+//         <p>Current Zoom: { this.state.zoom }</p>
 //       </div>
-//       )
+//       <div className='GMap-canvas' ref="mapCanvas">
+//       </div>
+//     </div>
+//   }
+
+//   componentDidMount() {
+//     // create the map, marker and infoWindow after the component has
+//     // been rendered because we need to manipulate the DOM for Google =(
+//     this.map = this.createMap()
+//     this.marker = this.createMarker()
+//     this.infoWindow = this.createInfoWindow()
+
+//     // have to define google maps event listeners here too
+//     // because we can't add listeners on the map until its created
+//     google.maps.event.addListener(this.map, 'zoom_changed', ()=> this.handleZoomChange())
+//   }
+
+//   // clean up event listeners when component unmounts
+//   componentDidUnMount() {
+//     google.maps.event.clearListeners(map, 'zoom_changed')
+//   }
+
+//   createMap() {
+//     let mapOptions = {
+//       zoom: this.state.zoom,
+//       center: this.mapCenter()
+//     }
+//     return new google.maps.Map(this.refs.mapCanvas, mapOptions)
+//   }
+
+//   mapCenter() {
+//     return new google.maps.LatLng(
+//       this.props.initialCenter.lat,
+//       this.props.initialCenter.lng
+//     )
+//   }
+
+//   createMarker() {
+//     return new google.maps.Marker({
+//       position: this.mapCenter(),
+//       map: this.map
+//     })
+//   }
+
+//   createInfoWindow() {
+//     let contentString = "<div class='InfoWindow'>I'm a Window that contains Info Yay</div>"
+//     return new google.maps.InfoWindow({
+//       map: this.map,
+//       anchor: this.marker,
+//       content: contentString
+//     })
+//   }
+
+//   handleZoomChange() {
+//     this.setState({
+//       zoom: this.map.getZoom()
+//     })
 //   }
 // }
 
+// var initialCenter = { lng: -90.1056957, lat: 29.9717272 }
+
+// ReactDOM.render(<GMap initialCenter={initialCenter} />, document.getElementById('root-container'));
+
+
+
 class App extends Component {
-//Below is only if you are using superagent
-  componentDidtMount() {
-    console.log('componentDidtMount')
+
+
+
+  constructor() {
+    super();
+
+    this.state = {
+      address: 'time square address'
+
+    };
+
   }
+
   render(){
     const location = {
       lat: 40.7575285,
@@ -39,12 +121,19 @@ class App extends Component {
     ]
     return (
       <div>
-mcndskjncsdjknc
-        <div style={{width:300, height:600, background: 'red'}}>
 
-          <Map center={location} markers={markers} />
+          <div style={{width:300, height:600, background: 'red'}}>
+
+             <Map center={location} markers={markers} />
+
+            <p>Location: { this.state.address }</p>
+
           </div>
-        </div>
+
+
+
+
+      </div>
 
       )
   }
