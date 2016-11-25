280a46e58a1f5bf18a142efb37923d9458bdde9a
Cecil Lee <cmjlee3@gmail.com>
2016-11-25 18:15:55 -0500

    map changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React    from 'react'
 import ReactDOM from 'react-dom'
 import App      from './Components/00App/App.jsx'
+// import {initialCenter} from './Components/00App/App.jsx'
 
 // mount our App at #container
 ReactDOM.render(<App />, document.querySelector('#root-container'));
